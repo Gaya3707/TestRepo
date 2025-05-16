39f064f94daf5f51b1b7a3521a84bb09a04c12ab
Gayathri Devi S <160008985+Gaya3707@users.noreply.github.com>
2025-05-16 23:55:02 +0530

    Create firstpyfile.py
    Just a python file

---
+++ firstpyfile.py
@@ -0,0 +1,3 @@
+#Print want I want
+print("This is a test repository, So I'm writing this to just get the hang of it")
+print("And I'm doing this because I'm taking the IBM Data Science Professional Certfication course")
